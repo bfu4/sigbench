a7ac8e6bac3edac561ee2a2c8f0de4165186f4ea
bfu4 <bella@sleeeepyti.me>
2021-04-06 16:21:33 -0700

    death

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
-module net.platinumdigitalgroup.sigbench {
+module sigbench {
 
 	requires javafx.fxml;
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.media;
+
+	opens net.platinumdigitalgroup.sigbench to javafx.fxml;
 
 	exports net.platinumdigitalgroup.sigbench;
 }
